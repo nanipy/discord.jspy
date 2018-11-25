ecd01c4c845d7699bb29096a7caec12f33a55903
Xua <williamblakelock.wb@gmail.com>
2018-11-25 23:11:35 +1300

    rewrite setup.py into jspy and add a license

--- setup.py
+++ setup.py
@@ -1,71 +1,73 @@
-from setuptools import setup, find_packages
-import re
+from js import Array, require, String;
 
-requirements = []
+setuptools = require("setuptools");
+re = require("re");
+
+requirements = Array();
 with open('requirements.txt') as f:
-  requirements = f.read().splitlines()
+  requirements = Array([String(f.read()).split("\n")]);
 
-version = ''
+version = String();
 with open('discord/__init__.py') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+    version = String(f.read()).search('^__version__\s*=\s*[\'"]([^\'"]*)[\'"]');
 
 if not version:
-    raise RuntimeError('version is not set')
+    raise RuntimeError('version is not set');
 
-if version.endswith(('a', 'b', 'rc')):
+if version.endsWith(Array([String('a'), String('b'), String('rc')])):
     # append version identifier based on commit count
     try:
-        import subprocess
-        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
+        subprocess = require("subprocess");
+        p = subprocess.Popen(Array([String('git'), String('rev-list'), String('--count'), String('HEAD')]),
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE);
+        out, err = p.communicate();
         if out:
-            version += out.decode('utf-8').strip()
-        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
+            version = version.concat(out.decode(String('utf-8')).strip());
+        p = subprocess.Popen(Array([String('git'), String('rev-parse'), String('--short'), String('HEAD')]),
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE);
+        out, err = p.communicate();
         if out:
-            version += '+g' + out.decode('utf-8').strip()
+            version = version.concat(String('+g').concat(String(out.decode('utf-8')).strip()));
     except Exception:
-        pass
+        pass;
 
-readme = ''
+readme = String();
 with open('README.rst') as f:
-    readme = f.read()
+    readme = String(f.read());
 
 extras_require = {
-    'voice': ['PyNaCl==1.2.1'],
-    'docs': [
-        'sphinx==1.7.4',
-        'sphinxcontrib-asyncio',
-        'sphinxcontrib-websupport',
-    ]
-}
+    String('voice'): Array([String('PyNaCl==1.2.1')]),
+    String('docs'): Array([
+        String('sphinx==1.7.4'),
+        String('sphinxcontrib-asyncio'),
+        String('sphinxcontrib-websupport'),
+    ])
+};
 
-setup(name='discord.py',
-      author='Rapptz',
-      url='https://github.com/Rapptz/discord.py',
+setup(name=String('discord.py'),
+      author=String('Rapptz'),
+      url=String('https://github.com/Rapptz/discord.py'),
       version=version,
-      packages=['discord', 'discord.ext.commands'],
-      license='MIT',
-      description='A python wrapper for the Discord API',
+      packages=Array([String('discord'), String('discord.ext.commands')]),
+      license=String('MIT'),
+      description=String('A python wrapper for the Discord API'),
       long_description=readme,
       include_package_data=True,
       install_requires=requirements,
       extras_require=extras_require,
-      python_requires='>=3.5.3',
-      classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
-      ]
-)
+      python_requires=String('>=3.5.3'),
+      classifiers=Array([
+        String('Development Status :: 4 - Beta'),
+        String('License :: OSI Approved :: MIT License'),
+        String('Intended Audience :: Developers'),
+        String('Natural Language :: English'),
+        String('Operating System :: OS Independent'),
+        String('Programming Language :: Python :: 3.5'),
+        String('Programming Language :: Python :: 3.6'),
+        String('Programming Language :: Python :: 3.7'),
+        String('Topic :: Internet'),
+        String('Topic :: Software Development :: Libraries'),
+        String('Topic :: Software Development :: Libraries :: Python Modules'),
+        String('Topic :: Utilities'),
+      ])
+);
